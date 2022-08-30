8460f9463807548d3842d50c9fd55768381fa709
Ruta Reisoglu <rutareiso@protonmail.com>
2022-08-30 19:09:22 -0400

    Separate templates in root and base foldrers

--- base/views.py
+++ base/views.py
@@ -3,10 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': "Let's learn Python!"},
+    {'id': 2, 'name': "Design with me"},
+    {'id': 3, 'name': "Frontend Devs!"}
+]
+
 
 def home(request):
-    return HttpResponse("Home page")
+    # pass data to template
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
 def room(request):
-    return HttpResponse('Room')
+    return render(request, 'base/room.html')
